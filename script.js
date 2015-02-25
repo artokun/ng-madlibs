321e1dce825a49f4c07c1433439dc5ae5b7bf8e5
Artokun <art.longbottom.jr@gmail.com>
2015-02-24 21:48:36 -0800

    added view switching

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
 angular.module('madLib', [])
-.controller('madLibCtrl', ['$scope', function($scope) {
-	
+  .controller('madLibCtrl', ['$scope', function ($scope) {
+    var female; //Neville I need help with the gender switcher!!
+    var gender = {};
+    var checkGender = function () {
+      female = $scope.female;
+      if (female == false) {
+        gender = {
+          gender: "Male",
+          he: "he",
+          his: "his",
+          him: "him",
+          female: false
+        };
+      } else {
+        gender = {
+          gender: "Female",
+          he: "she",
+          his: "her",
+          him: "her",
+          female: true
+        };
+      }
+      return gender;
+    };
+    $scope.gender = checkGender();
+    
+    $scope.resetForm = function() {
+      $scope.user = {};
+      $scope.user.$setPristine();
+    };
 }]);
-
